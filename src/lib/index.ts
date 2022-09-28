fc22ba0a32fca3792a7c9ecbec654039695e50aa
Badu Blanc <badu@hey.com>
2022-09-27 21:20:35 -0500

    basic jsdocs

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -36,22 +36,77 @@ export class Factory {
   }
 
   utils = {
-    getSeedFromBlockHash: (id: BigNumberish, blockHash?: string) => {
+    /** Emulates Noun.sol to generate a pseudorandom seed
+     * @param {BigNumberish} id
+     * @param {string} [blockHash]
+     */
+    getSeedFromBlockHash: (id: BigNumberish, blockHash?: string): Seed => {
       if (!blockHash) {
         blockHash =
           "0x305837d283efbc5a8ea53934fb122ac88473c68c1db0ebe2a2279f09f5772878";
       }
       return helpers.getSeedFromBlockHash(id, blockHash, this.layers);
     },
-    getRandomSeed: () => helpers.getRandomSeed(this.layers),
-    arrayToSeed: (arr: number[]) => helpers.arrayToSeed(arr, this.layers),
-    arrayToTraits: (arr: number[]) => helpers.arrayToTraits(arr, this.layers),
-    seedToArray: (seed: Seed) => helpers.seedToArray(seed, this.layers),
-    seedToTraits: (seed: Seed) => helpers.seedToTraits(seed, this.layers),
-    traitsToSeed: (traits: Traits | PartialTraits) =>
-      helpers.traitsToSeed(traits, this.layers),
-    traitsToArray: (traits: Traits | PartialTraits) =>
-      helpers.traitsToArray(traits, this.layers),
-    validateSeed: (seed: Seed) => helpers.validateSeed(seed, this.layers),
+
+    /** Generates a random seed */
+    getRandomSeed: (): Seed => {
+      return helpers.getRandomSeed(this.layers);
+    },
+
+    /** Transforms seed array into a seed object
+     * @param {number[]} arr
+     * @returns Seed
+     */
+    arrayToSeed: (arr: number[]): Seed => {
+      return helpers.arrayToSeed(arr, this.layers);
+    },
+
+    /** Transforms seed array into a traits object
+     * @param {number[]} arr
+     * @returns Traits
+     */
+    arrayToTraits: (arr: number[]): Traits => {
+      return helpers.arrayToTraits(arr, this.layers);
+    },
+
+    /** Transforms seed object into a seed array
+     * @param {Seed} seed
+     * @returns number[]
+     */
+    seedToArray: (seed: Seed): number[] => {
+      return helpers.seedToArray(seed, this.layers);
+    },
+
+    /** Transforms seed object into a traits object
+     * @param {Seed} seed
+     * @returns Traits
+     */
+    seedToTraits: (seed: Seed): Traits => {
+      return helpers.seedToTraits(seed, this.layers);
+    },
+
+    /** Transforms traits object into a seed object
+     * @param {Traits|PartialTraits} traits
+     * @returns Seed
+     */
+    traitsToSeed: (traits: Traits | PartialTraits): Seed => {
+      return helpers.traitsToSeed(traits, this.layers);
+    },
+
+    /** Transforms traits object into a seed array
+     * @param {Traits|PartialTraits} traits
+     * @returns number[]
+     */
+    traitsToArray: (traits: Traits | PartialTraits): number[] => {
+      return helpers.traitsToArray(traits, this.layers);
+    },
+
+    /** Confirms validity of given seed
+     * @param {Seed} seed
+     * @throws if provided Seed has unknown or missing keys, or value can't be located
+     */
+    validateSeed: (seed: Seed): void => {
+      return helpers.validateSeed(seed, this.layers);
+    },
   };
 }
