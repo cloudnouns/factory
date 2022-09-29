0eb6655e9bd536ffe5bd72ef3839e860ca8f694a
Badu Blanc <badu@hey.com>
2022-09-28 19:52:11 -0500

    cli testing

--- cli.ts
+++ cli.ts
@@ -1,19 +1,31 @@
 #! /usr/bin/env node
+import type { BoltConfig } from "./src/types";
 import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import Config from "./bolt.config.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+console.log(Config.items);
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const args = process.argv.slice(2, process.argv.length);
 const action = args[0];
 
-// const path =
-
 if (action === "init") {
   console.log("Looking for config file...");
-  console.log(__dirname);
+  const file = "./bolt.config.js";
+
+  if (fs.existsSync(file)) {
+    console.log("found!");
+    const config = fs.readFileSync(file, "utf8");
+    console.log(config);
+    // const file2 = ((await import("./bolt.config.js")) as BoltConfig).default;
+    // console.log(file2);
+  } else {
+    console.log("not found. create?");
+  }
 
   /**
    * check for dir
