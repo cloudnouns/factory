5087f8cecc4afc77fb98b0e71b057bb3eac2ebcd
Badu Blanc <badu@hey.com>
2022-09-30 18:02:13 -0500

    generate types in .bolt dir

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import toml from "@ltd/j-toml";
 
 type ConfigFile = {
@@ -17,7 +17,7 @@ type ImageData = {
   images: { [key: string]: any[] };
 };
 
-const typeTemplate = `export type {ITEM_NAME}Parts = {
+const template = `export type {ITEM_NAME}Parts = {
 	**OBJECT_DEFINITIONS**
 };
 
@@ -29,35 +29,60 @@ export type {ITEM_NAME}BgColors = **BG_COLORS**;
 export const readConfigAndGenerateTypes = async (
   path: string = "bolt.toml"
 ) => {
-  if (!fs.existsSync(path)) {
-    throw new Error("Unable to read or find config file");
+  if (!existsSync(path)) {
+    throw new Error("Unable to find or read config file");
   }
 
-  const config = fs.readFileSync(path, "utf-8");
+  const config = readFileSync(path, "utf-8");
   let { items } = toml.parse(config) as unknown as ConfigFile;
-  if (items.length) {
-    items.forEach((item) => {
-      const imageData = JSON.parse(fs.readFileSync(item.config_path, "utf-8"));
-      generateTypes(item.name, imageData);
-    });
+
+  if (!existsSync("./.bolt")) mkdirSync("./.bolt");
+
+  for (const item of items) {
+    const imageData = JSON.parse(readFileSync(item.config_path, "utf-8"));
+    const { path, content } = generateTypes(item.name, imageData);
+    writeFileSync(path, content);
+  }
+
+  if (existsSync("tsconfig.json")) {
+    const config = JSON.parse(readFileSync("tsconfig.json", "utf-8"));
+    let { compilerOptions } = config;
+
+    if (compilerOptions.typeRoots) {
+      const { typeRoots } = compilerOptions;
+      if (typeRoots.includes("./.bolt")) return;
+      compilerOptions.typeRoots.push("./.bolt");
+    } else {
+      compilerOptions = {
+        typeRoots: ["node_modules/@types", "./.bolt"],
+        ...compilerOptions,
+      };
+    }
+
+    config.compilerOptions = compilerOptions;
+    writeFileSync("tsconfig.json", JSON.stringify(config, null, 2));
+  } else {
+    console.log('TSConfig not found. Remember to add "./.bolt" to typeRoots');
   }
 
   return;
 };
 
-const generateTypes = (item: string, imageData: ImageData, outDir?: string) => {
-  if (!outDir) outDir = "./.bolt/";
+const generateTypes = (item: string, imageData: ImageData) => {
+  item = item.replace(/ /g, "");
+  const path = `./.bolt/${item.toLowerCase()}.ts`;
   const { bgcolors, images } = imageData;
-  const imageKeys = Object.keys(images);
+  const parts = Object.keys(images);
 
-  const entries = imageKeys.map((key) => {
+  const entries = parts.map((part) => {
     const entry = {
-      trait: key,
-      label: key.charAt(0).toUpperCase() + key.slice(1).toLowerCase() + "Layer",
+      part,
+      label:
+        part.charAt(0).toUpperCase() + part.slice(1).toLowerCase() + "Part",
       types: "",
     };
 
-    entry.types = images[key]
+    entry.types = images[part]
       .map((data) => {
         return '"' + data.filename.toLowerCase().replace(/ /g, "-") + '"';
       })
@@ -72,19 +97,18 @@ const generateTypes = (item: string, imageData: ImageData, outDir?: string) => {
 
   const definitions = {
     object: entries
-      .map((entry) => `${entry.trait}: ${entry.label};`)
+      .map((entry) => `${entry.part}: ${entry.label};`)
       .join("\n\t"),
     individual: entries
       .map((entry) => `export type ${entry.label} =\n\t| ${entry.types};`)
       .join("\n\n"),
   };
 
-  const generated = typeTemplate
+  const content = template
     .replace(/{ITEM_NAME}/g, item)
     .replace("**BG_COLORS**", bgColors)
     .replace("**OBJECT_DEFINITIONS**", definitions.object)
     .replace("**INDIVIDUAL_DEFINITIONS**", definitions.individual);
 
-  const outFile = `./src/types/${item.toLowerCase().replace(/ /g, "-")}.ts`;
-  fs.writeFileSync(outFile, generated);
+  return { path, content };
 };
